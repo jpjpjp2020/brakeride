dbc5a054ec7356995a9503a9d94fd020ebf76524
Jorgen Post <jrgnpst@gmail.com>
2023-08-23 16:37:13 +0300

    audio_utils setup for modularity

---
+++ dashboard/audio_utils.py
@@ -0,0 +1,23 @@
+AUDIO_ACTIONS = {
+    # general audio commands:
+    'start': 'audio/start.mp3',
+    'end': 'audio/end.mp3',
+    # emergency practice audio commands:
+    'brake': 'audio/brake.mp3',
+    'swerve_right': 'audio/swerve_right.mp3',
+    'swerve_left': 'audio/swerve_left.mp3',
+    # city/traffic practice audio commands:
+    'spot_a_hazard': 'audio/spot_a_hazard.mp3',
+    'find_and_escape': 'audio/find_and_escape.mp3',
+    'spot_and_find': 'audio/spot_and_find.mp3',
+}
+
+
+def generate_schedule_for_emergency(user_choices):
+    # Generate a schedule specific to emergency practice mode
+    pass
+
+
+def generate_schedule_for_city_traffic(user_choices):
+    # Generate a schedule specific to city/traffic mode
+    pass
