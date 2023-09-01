cbbae403fa3f22f3f81bac5960e0b3c4451502cf
Jorgen Post <jrgnpst@gmail.com>
2023-09-02 02:21:47 +0300

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./dashboard/templates/**/*.html', './dashboard/static/**/*.js'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace']
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit',
+}
