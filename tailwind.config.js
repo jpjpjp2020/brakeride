af3d18f275ff56b2c1d438f07dd39da437a4874c
Jorgen Post <jrgnpst@gmail.com>
2023-09-17 01:46:18 +0300

    v.1 working code with debug setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       animation: {
         'spin-slow': 'spin 3s linear infinite',
-      }
+      },
+      width: {
+        '3/10': '30%',
+      },
     },
     fontFamily: {
       'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
